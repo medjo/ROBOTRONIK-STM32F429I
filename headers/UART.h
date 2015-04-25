8a6748e6239bf36fc9f4ae7f519aa46d9fcf6d03
mejdo <ikhalo.o@hotmail.fr>
2015-04-25 17:05:52 +0200

    ajout fonction init uart asser non testée !

--- headers/UART.h
+++ headers/UART.h
@@ -2,17 +2,14 @@
 #define __UART_H__
 
 #include "main.h"
-void init_UART(UART_HandleTypeDef *UART_HandleStructure);
 
 /* UART handler declarations */
 UART_HandleTypeDef	UART_HandleStructure_T;
 UART_HandleTypeDef	UART_HandleStructure_R;
 
-UART_HandleTypeDef	UART_Asser_T;
-UART_HandleTypeDef	UART_Asser_R;
+UART_HandleTypeDef	UART_Asser;//PD8, USART3_TX PD9,	USART3_RX
 
-UART_HandleTypeDef	UART_Xbee_T;
-UART_HandleTypeDef	UART_Xbee_R;
+UART_HandleTypeDef	UART_Xbee;
 
 void init_UART(UART_HandleTypeDef *UART_HandleStructure);
 void send_word(UART_HandleTypeDef *UART_HandleStructure, const char *str);
--- src/UART.c
+++ src/UART.c
@@ -1,5 +1,10 @@
 #include <UART.h>
+#include <string.h>
 
+void USART3_IRQHandler(void)//cf. startup_stm32f407xx.s
+{
+	HAL_UART_IRQHandler(&UART_Asser);
+}
 
 void UART4_IRQHandler(void)
 {
@@ -18,6 +23,9 @@ void Error_UART(){
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
 	tmp_bfr_test_UART = *RX_bfr_test_UART;
 }
+
+//void UART_handler_
+
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
 	reset_all_led();
 	if (*RX_bfr_test_UART == tmp_bfr_test_UART) {
@@ -25,8 +33,6 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
 	} else {
 		pas_recu ();
 	}
-	
-	
 }
 
 void bien_recu () {
@@ -174,7 +180,7 @@ void test_UART_TX_RX(){
 	
 	send_word_IT(&UART_HandleStructure_T, "\n\n\rDemande la lettre ");
 	Delay(50);
-	send_word_IT(&UART_HandleStructure_T, RX_bfr_test_UART);
+	send_word_IT(&UART_HandleStructure_T, (const char *)RX_bfr_test_UART);
 	if(HAL_UART_Receive_IT(&UART_HandleStructure_T, RX_bfr_test_UART, 1) != HAL_OK)
 	{
 		HAL_GPIO_WritePin(GPIOD, LED_ROUGE, GPIO_PIN_SET);
@@ -188,3 +194,49 @@ void test_UART_TX_RX(){
 	}
 }
 
+void init_UART_Asser(UART_HandleTypeDef *UART_HandleStructure) {
+	
+	__USART3_CLK_ENABLE(); //cf. stm32f4xx_hal_rcc_ex.h
+	
+		
+	//##-3- Configure the NVIC for UART ########################################
+	HAL_NVIC_SetPriority(USART3_IRQn, 4, 4);
+	HAL_NVIC_EnableIRQ(USART3_IRQn);
+	
+	
+	//Configuration de l'UART d'émission
+	UART_HandleStructure->Init.BaudRate = 9600;//1;//4095
+	UART_HandleStructure->Init.WordLength = UART_WORDLENGTH_8B;
+	UART_HandleStructure->Init.StopBits = UART_STOPBITS_1;
+	UART_HandleStructure->Init.Parity = UART_PARITY_NONE; //ou UART_PARITY_EVEN ou UART_PARITY_ODD
+	UART_HandleStructure->Init.Mode = UART_MODE_TX_RX; // ou UART_MODE_RX ou UART_MODE_TX_RX
+	UART_HandleStructure->Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	UART_HandleStructure->Init.OverSampling = UART_OVERSAMPLING_8;
+	UART_HandleStructure->Instance = USART3; // cf. stm32f407xx.h
+	
+	
+	
+	//HAL_UART_Init(UART_HandleStructure);
+	if(HAL_UART_Init(UART_HandleStructure) != HAL_OK)
+	{
+		Error_UART();
+	}
+	
+	
+	//AF7 : Pin D8 : USART3_TX
+	__GPIOD_CLK_ENABLE();
+	
+	
+	//Configuration du pin de transmission
+    GPIO_InitTypeDef GPIO_InitStructure_UART;
+
+    GPIO_InitStructure_UART.Pin = GPIO_PIN_8|GPIO_PIN_8;
+    GPIO_InitStructure_UART.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStructure_UART.Speed = GPIO_SPEED_HIGH;
+    GPIO_InitStructure_UART.Pull = GPIO_PULLUP;
+    GPIO_InitStructure_UART.Alternate = GPIO_AF7_USART3;//cf. stm32f4xx_hal_gpio_ex.h
+    
+    //Configure this pin in Alternate function mode
+    HAL_GPIO_Init(GPIOD, &GPIO_InitStructure_UART);
+	
+}
